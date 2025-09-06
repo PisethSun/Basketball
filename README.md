354d361bd43fb0b477429d673ab2465293f0d8d0
Piseth Sun <35794825+PisethSun@users.noreply.github.com>
2025-09-06 01:06:54 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+ Solo Project - I did not use any AI. Please feel free to give any feedback
+
+ UI link: https://www.figma.com/design/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0-1&p=f&t=higYUbh7ZbNiZmdn-0
+
+Bonus: 
+
+ I added Reset button targeting each board. 
---
+++ index.js
@@ -0,0 +1,72 @@
+let homeScores = 0 
+let guestScores = 0
+document.getElementById("homeScore").textContent=homeScores
+document.getElementById("guestScore").textContent=guestScores
+
+
+let home = document.getElementById("homeScore")
+let guest = document.getElementById("guestScore")
+let resetBtn = document.getElementById("reset-btn")
+
+// home one function
+function addOne(){
+
+    homeScores += 1
+
+    home.textContent = homeScores
+
+}
+
+function addTwo(){
+
+      homeScores += 2
+
+    home.textContent = homeScores
+}
+
+function addThree(){
+     homeScores += 3
+
+    home.textContent = homeScores
+
+}
+
+
+// Guest ScORE
+
+
+
+// home one function
+function addoneGuest(){
+
+    guestScores += 1
+
+    guest.textContent = guestScores
+
+}
+
+function addtwoGuest(){
+
+      guestScores += 2
+
+    guest.textContent = guestScores
+}
+
+function addthreeGuest(){
+     guestScores += 3
+
+    guest.textContent = guestScores
+}
+
+
+function resetHome() {
+    homeScores = 0;
+   
+    home.textContent = homeScores;
+}
+
+function resetGuest(){
+     guestScores = 0;
+
+    guest.textContent = guestScores;
+}
