a8bd0e321c339ccd61d3e14cf67d2af7d848f1b1
Piseth Sun <35794825+PisethSun@users.noreply.github.com>
2025-09-06 01:50:57 -0400

    changes a lot becuase Netify didnt work. ID and classes might be the issue

--- index.js
+++ index.js
@@ -1,72 +1,47 @@
-let homeScores = 0 
-let guestScores = 0
-document.getElementById("homeScore").textContent=homeScores
-document.getElementById("guestScore").textContent=guestScores
+let homeScores = 0;
+let guestScores = 0;
 
+let home = document.getElementById("homeScore");
+let guest = document.getElementById("guestScore");
 
-let home = document.getElementById("homeScore")
-let guest = document.getElementById("guestScore")
-
-
-// home one function
-function addOne(){
-
-    homeScores += 1
-
-    home.textContent = homeScores
-
-}
-
-function addTwo(){
-
-      homeScores += 2
-
-    home.textContent = homeScores
-}
-
-function addThree(){
-     homeScores += 3
-
-    home.textContent = homeScores
-
-}
-
-
-// Guest ScORE
-
-
-
-// home one function
-function addoneGuest(){
-
-    guestScores += 1
-
-    guest.textContent = guestScores
-
-}
-
-function addtwoGuest(){
-
-      guestScores += 2
-
-    guest.textContent = guestScores
-}
-
-function addthreeGuest(){
-     guestScores += 3
-
-    guest.textContent = guestScores
-}
-
-
-function resetHome() {
-    homeScores = 0;
-   
+// HOME FUNCTIONS
+function addOne() {
+    homeScores += 1;
     home.textContent = homeScores;
 }
 
-function resetGuest(){
-     guestScores = 0;
+function addTwo() {
+    homeScores += 2;
+    home.textContent = homeScores;
+}
 
+function addThree() {
+    homeScores += 3;
+    home.textContent = homeScores;
+}
+
+function resetHome() {
+    homeScores = 0;
+    home.textContent = homeScores;
+}
+
+// GUEST FUNCTIONS
+function addOneGuest() {
+    guestScores += 1;
+    guest.textContent = guestScores;
+}
+
+function addTwoGuest() {
+    guestScores += 2;
+    guest.textContent = guestScores;
+}
+
+function addThreeGuest() {
+    guestScores += 3;
+    guest.textContent = guestScores;
+}
+
+function resetGuest() {
+    guestScores = 0;
     guest.textContent = guestScores;
 }
